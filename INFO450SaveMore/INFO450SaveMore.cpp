339615ebed663ba4d0d7233310b1ea0351cea935
James Nicholson <nicholsonjb@vcu.edu>
2017-03-17 15:54:24 -0400

    Account Class Created

--- INFO450SaveMore/INFO450SaveMore.cpp
+++ INFO450SaveMore/INFO450SaveMore.cpp
@@ -2,6 +2,23 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+class Account
+{
+	float accountNum;
+	float interestRate;
+	float balance;
+
+public:
+		void withdraws();
+		void deposits();
+
+		
+		virtual void display() = 0;		
+		
+};
+
 
 
 int main()
