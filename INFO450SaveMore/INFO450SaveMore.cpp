c149702791043cac42196b5d16d297f9bb002b27
nicholsonjb <nicholsonjb@vcu.edu>
2017-03-24 15:47:34 -0400

    CD class updated and dispaly added

--- INFO450SaveMore/INFO450SaveMore.cpp
+++ INFO450SaveMore/INFO450SaveMore.cpp
@@ -23,12 +23,12 @@ public:
 		balance = b;
 	}
 
-//Method Bank Account
-	virtual int withdraw(float wAmount)=0;
+	//Method Bank Account
+	virtual int withdraw(float wAmount) =0;
 	virtual float deposit(float dAmount) =0;
 	virtual float getBalance() = 0;
 	virtual float calculateIntrest() = 0;
-	virtual float orderChecks()=0;
+	virtual float orderChecks() =0;
 	virtual void Display() =0;
 };
 
@@ -37,14 +37,16 @@ public:
 class savingsAccount : public bankAccount
 {
 public:
-	savingsAccount(int a, float i, float b) : bankAccount(a,i, b)
-	{}
+	savingsAccount(int a, float i, float b) : bankAccount(a, i, b)
+	{
+	}
+
 	//Method Savings Accouunt withdraw
 	int withdraw(float wAmount)
 	{
 		if (balance - wAmount >= 0)
 		{
-			balance = (balance -2) - wAmount;
+			balance = (balance - 2) - wAmount;
 			return 0;
 		}
 		else
@@ -54,10 +56,8 @@ public:
 	//Method Savings Account Deposit
 	float deposit(float dAmount)
 	{
-		
-		balance  = balance + dAmount;
+		balance = balance + dAmount;
 		return balance;
-		
 	}
 
 	//Get Balance for Saving
@@ -65,20 +65,20 @@ public:
 	{
 		return balance;
 	}
+
 	//monthly intrest for savings
 	float calculateIntrest()
 	{
 		if (balance < 10000)
 		{
-			balance = balance*pow((1 + .01 / 12), 12);
+			balance = balance * pow((1 + .01 / 12), 12);
 			return balance;
 		}
 		if (balance >= 10000)
 		{
-			balance = balance*pow((1 + .02 / 12), 12);
+			balance = balance * pow((1 + .02 / 12), 12);
 			return balance;
-		}
-		;
+		};
 	}
 
 	//order checks
@@ -93,32 +93,28 @@ public:
 		cout << "Savings Account Number: " << accountNumber << endl;
 		cout << "Savings Account Bal: " << balance;
 	}
-
-
 };
 
 //Checcking Class with Inheritance from account
 class checkingAccount : public bankAccount
 {
-
 public:
 
-	checkingAccount(int a, float i, float b) : bankAccount(a, i, b){}
-	
+	checkingAccount(int a, float i, float b) : bankAccount(a, i, b)
+	{
+	}
+
 	//Method checking Accouunt withdraw
 	int withdraw(float wAmount)
 	{
 		if (balance - wAmount >= 0)
 		{
 			balance = balance - wAmount;
-			
-			
 		}
 		if (balance < 500)
-		 {
+		{
 			balance = (balance - 5);
-			 
-		 }
+		}
 		else
 			return -1;
 	}
@@ -126,10 +122,8 @@ public:
 	//Method checking Account Deposit
 	float deposit(float dAmount)
 	{
-
 		balance = balance + dAmount;
 		return balance;
-
 	}
 
 	//Get Balance for Saving
@@ -143,18 +137,19 @@ public:
 		balance = balance - 15;
 		return balance;
 	}
+
 	//monthly intrest for checking
 	float calculateIntrest()
 	{
 		return 0;
 	}
+
 	//Display checking Account
 	void Display()
 	{
 		cout << "Checking Account Number: " << accountNumber << endl;
 		cout << "Checking Account Bal: " << balance;
 	}
-
 };
 
 
@@ -163,18 +158,22 @@ class certificateofDeposit : public bankAccount
 {
 	int term;
 public:
-	certificateofDeposit(int a, float i, float b) : bankAccount(a, i, b)
+	certificateofDeposit(int a, float i, float b, int t) : bankAccount(a, i, b)
 
-	{}
+	{
+		term = t;
+	}
+
 	//Method Savings Accouunt withdraw
+
 	int withdraw(float wAmount)
 	{
 		if (balance - wAmount >= 0)
 		{
-			balance = (balance *(1 + .1)) - wAmount;
+			balance = (balance - (balance * .1)) - wAmount;
 			return 0;
 		}
-	
+
 		else
 			return -1;
 	}
@@ -182,10 +181,8 @@ public:
 	//Method CD Account Deposit
 	float deposit(float dAmount)
 	{
-
 		balance = balance + dAmount;
 		return balance;
-
 	}
 
 	//Get Balance for CD
@@ -199,18 +196,20 @@ public:
 	{
 		if (term < 5)
 		{
-			balance = balance*pow((1 + .1 / 12), 12);
+			balance = balance * pow((1 + .1 / 12), 12);
 			return balance;
 		}
-		if (term > 5)
+		if (term >= 5)
 		{
-			balance = balance*pow((1 + .5 / 12), 12);
+			balance = balance * pow((1 + .5 / 12), 12);
 			return balance;
 		}
-		
 	}
 
-	float orderChecks();
+	float orderChecks()
+	{
+		return 0;
+	}
 
 	//Display  CD Account
 	void Display()
@@ -224,69 +223,75 @@ public:
 int main()
 {
 	int nBankAccounts = 10;
-	
-		
-		bankAccount **acc = new bankAccount*[nBankAccounts];
-		acc[0] = new savingsAccount(100,0,0); //Open Savings
-		acc[1] = new checkingAccount(101, 0, 0); //Open Checking
-		
-	//Savings Account Deposit
-		
-		acc[0]->deposit(10000);
-		
-		
-	//Checking account deposit
-		acc[1]->deposit(600);
 
-		//displacy savings, checking, cd
-		cout << "Deposit Savings" << endl;
-		acc[0]->Display();
-		cout << "\n" << endl;
-		cout << "Deposit Checking" << endl;
-		acc[1]->Display();
+	//allocate memory for bankaccount
+	bankAccount** acc = new bankAccount*[nBankAccounts]; 
+
+	acc[0] = new savingsAccount(100, 0, 0); //Open Savings
+	acc[1] = new checkingAccount(101, 0, 0); //Open Checking
+	acc[2] = new certificateofDeposit(102, 0, 0, 3); //Open CD
+
+	//Savings Account Deposit
+
+	acc[0]->deposit(10000);
+
+
+	//Checking account deposit
+	acc[1]->deposit(600);
+
+	//CD Deposit
+	acc[2]->deposit(10000);
+
+	//displacy savings, checking, cd
+	cout << "Deposit Savings:" << endl;
+	acc[0]->Display();
+	cout << "\n" << endl;
+	cout << "Deposit Checking:" << endl;
+	acc[1]->Display();
+	cout << "\n" << endl;
+	cout << "Deposit CD:" << endl;
+	acc[2]->Display();
 
 	//Savings Intresest
-		cout << "\n\n---------\n";
-		cout << "Savings Intrest" << endl;
-		acc[0]->calculateIntrest();
-		acc[0]->Display();
-	
-		//Order checks
-		cout << "\n---------\n" << endl;
-		cout << "Order Checks" << endl;
-		acc[1]->orderChecks();
-		acc[1]->Display();
+	cout << "\n\n---------\n";
+	cout << "Savings Intrest:" << endl;
+	acc[0]->calculateIntrest();
+	acc[2]->calculateIntrest();
+	acc[0]->Display();
+	cout << "\n\nCD Intrest:" << endl;
+	acc[2]->Display();
+
+	//Order checks
+	cout << "\n\n---------\n" << endl;
+	cout << "Order Checks:" << endl;
+	acc[1]->orderChecks();
+	acc[1]->Display();
 
 	//Savings Withdraw
-		cout << "\n\n---------\n" << endl;
-		cout << "Withdraw Savings" << endl;
-		acc[0]->withdraw(1000);
-		acc[0]->Display();
-		//Saving Checking
-		cout << "\n" << endl;
-		cout << "Withdraw Checking" << endl;
-		acc[1]->withdraw(200);
-		acc[1]->Display();
-		
+	cout << "\n\n---------\n" << endl;
+	cout << "Withdraw Savings:" << endl;
+	acc[0]->withdraw(1000);
+	acc[0]->Display();
 
-	
-		
+	//Withdraw Checking
+	cout << "\n" << endl;
+	cout << "Withdraw Checking:" << endl;
+	acc[1]->withdraw(200);
+	acc[1]->Display();
 
-
-		
-
-
-		//acc[2] = new certificateofDeposit(300, 0, 10000, 10000, 3); //Open CD
-		//acc[2]->Display();											//Display CD
+	//withdraw CD
+	cout << "\n" << endl;
+	cout << "Withdraw CD:" << endl;
+	acc[2]->withdraw(2000);
+	acc[2]->Display();
+	cout << "\n" << endl;
 
 
-
-		delete acc[0];
-		delete acc[1];
-		//delete acc[2];
-		delete[] acc;
+	//clear memory
+	delete acc[0];
+	delete acc[1];
+	delete acc[2];
+	delete[] acc;
 
 	return 0;
 }
-
- 
